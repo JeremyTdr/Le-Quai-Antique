976dcbde45ed2a906108267a9339d1d5e4233a86
Jeremy Tdr <jeremy.tdr@gmail.com>
2023-02-14 11:14:11 +0100

    Navbar mockup v1

--- pages/includes/navbar.php
+++ pages/includes/navbar.php
@@ -1,16 +1,15 @@
 <nav class="nav">
-    <div class="nav-left">
-        <ul class="nav-items">
-            <li class="nav-item"><img class="logo" src="src/assets/Logo.svg" alt="logo du restaurant"></img></li>
-            <li class="nav-item">Accueil</li>
-            <li class="nav-item">Carte</li>
-            <li class="nav-item">Informations</li>
-        </ul>
+
+    <div class="navbar">
+        <img class="logo" src="src/assets/Logo.svg" alt="logo du restaurant"></img>
+        <i class="fas fa-bars"></i>
     </div>
-    <div class="nav-right">
-        <ul class="nav-items">
-            <li class="nav-item">Reserver</li>
-            <li class="nav-item"><i class="fas fa-user"></i></li>
-        </ul>
-    </div>
+
+    <ul class="nav-items">
+        <li class="nav-item">Accueil</li>
+        <li class="nav-item">Carte</li>
+        <li class="nav-item">Informations</li>
+        <li class="nav-item reserved">Reserver</li>
+        <li class="nav-item"><i class="fas fa-user"></i></li>
+    </ul>
 </nav>
