4091c0f0c7ad363ae303e4139c5ef8b32073f687
Jeremy Tdr <jeremy.tdr@gmail.com>
2023-04-16 15:15:03 +0200

    Contact section created - Mobile & Desktop

--- index.php
+++ index.php
@@ -3,5 +3,6 @@
 <?php include './pages/main.php'; ?>
 <?php include './pages/presentation.php'; ?>
 <?php include './pages/bestDishes.php'; ?>
+<?php include './pages/contact.php'; ?>
 
 <?php include './pages/includes/html-foot.php'; ?>
---
+++ pages/contact.php
@@ -0,0 +1,22 @@
+<secion class="contact" id="contact">
+    <div class="content">
+        <h2>Contact & Accès</h2>
+        <div class="contact-items">
+            <div class="contact-infos">
+                <img class="logo" src="src/assets/Logo.svg" alt="logo du restaurant"></img>
+                <ul class="infos-items">
+                    <li class="infos-item"><i class="fas fa-map-marker-alt"></i>1 av. de Paris - 75000, Paris</li>
+                    <li class="infos-item"><i class="fas fa-mobile-alt"></i>01 02 03 04 05</li>
+                    <li class="infos-item"><i class="fas fa-envelope"></i>contact@quaiantique.fr</li>
+                </ul>
+                <div class="reseaux">
+                    <a href="https://www.instagram.com/" id="instagram"><i class="fab fa-instagram"></i></a>
+                    <a href="https://www.facebook.com/" id="facebook"><i class="fab fa-facebook-square"></i></a>
+                </div> 
+            </div>
+            <div class="contact-map">
+                <iframe class="google-maps" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d83998.96777928845!2d2.26463436180185!3d48.85882549197555!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66e1f06e2b70f%3A0x40b82c3688c9460!2sParis!5e0!3m2!1sfr!2sfr!4v1681649467597!5m2!1sfr!2sfr" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+        </div>
+    </div>
+</secion>
